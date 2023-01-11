36e6b4dafb5e146958ee7219eb57df7aa3260547
Tuvshinbayar Tuvshinzul <tuvshinbayar@hexagon.mn>
2023-01-11 15:54:22 +0800

    FIX: rate

--- hummingbot/core/rate_oracle/sources/coinhub_rate_source.py
+++ hummingbot/core/rate_oracle/sources/coinhub_rate_source.py
@@ -5,6 +5,7 @@ from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
 from hummingbot.core.api_throttler.data_types import RateLimit
 from hummingbot.core.rate_oracle.sources.rate_source_base import RateSourceBase
 from hummingbot.core.utils import async_ttl_cache
+from hummingbot.core.utils.async_utils import safe_gather
 from hummingbot.core.web_assistant.connections.data_types import RESTMethod
 from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
 
@@ -31,15 +32,22 @@ class CoinhubRateSource(RateSourceBase):
         :return A dictionary of trading pairs and prices
         """
         results = {}
-        fiats = await self.get_coinhub_prices()
-        if isinstance(fiats, Exception):
-            self.logger().error(fiats)
-            self.logger().error(
-                "Unexpected error while retrieving rates from Coinhub. " "Check the log file for more info."
-            )
-        else:
-            print()
-            results.update(fiats)
+        tasks = [
+            self.get_coinhub_prices(),
+            self.get_coinbase_prices_by_currency("USD"),
+        ]
+        task_results = await safe_gather(*tasks, return_exceptions=True)
+        for task_result in task_results:
+            if isinstance(task_result, Exception):
+                self.logger().error(task_result)
+                self.logger().error(
+                    "Unexpected error while retrieving rates from Coinbase. " "Check the log file for more info."
+                )
+                break
+            else:
+                results.update(task_result)
+        if "MNT-USD" in results and "USD-USDT" in results:
+            results["USDT-MNT"] = (1 / results["MNT-USD"]) * results["USD-USDT"]
         return results
 
     async def get_coinhub_prices(self) -> Dict[str, Decimal]:
@@ -60,6 +68,35 @@ class CoinhubRateSource(RateSourceBase):
         )
         rates = request_result["data"]
         for rate in rates:
+            if rate["stock"] == "USDT":
+                continue
             pair = f"{rate['stock'].upper()}-{rate['money'].upper()}"
             results[pair] = Decimal(f"{rate['price']}")
+        return results
+
+    async def get_coinbase_prices_by_currency(self, currency: str) -> Dict[str, Decimal]:
+        """
+        Fetches Coinbase exchange rates.
+        :param vs_currency: A currency (crypto or fiat) to get prices of tokens in, see
+        https://api.coinbase.com/v2/currencies for the current supported list
+        :param currency
+        :return A dictionary of fiat rates
+        """
+        results = {}
+        rest_assistant = await self._api_factory.get_rest_assistant()
+        price_url = "https://api.coinbase.com/v2/exchange-rates"
+        request_result = await rest_assistant.execute_request(
+            method=RESTMethod.GET,
+            url=price_url,
+            throttler_limit_id=REST_CALL_RATE_LIMIT_ID,
+            params={
+                "currency": currency
+            }
+        )
+        rates = request_result["data"]["rates"]
+        for quote, price in rates.items():
+            if quote == "MNT":
+                continue
+            pair = f"{currency}-{quote.upper()}"
+            results[pair] = Decimal(str(price))
         return results
